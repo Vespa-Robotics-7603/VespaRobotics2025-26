d72e2a749d4cdd884bc17746a753007eaa2f2862
Broclly <149013895+Broclly@users.noreply.github.com>
2024-12-09 09:28:39 -0500

    code corrected (don't worry abt this)

--- Robot.java
+++ Robot.java
@@ -1,96 +1,82 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-
+import java.util.List;
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.UsbCamera;
-import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.apriltag.AprilTagDetector;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+  private VictorSPX driveLeft1 = new VictorSPX(1);
+  private VictorSPX driveRight1 = new VictorSPX(3);
+  private VictorSPX driveLeft2 = new VictorSPX(2);
+  private VictorSPX driveRight2 = new VictorSPX(4);
+  private Joystick joystick = new Joystick(0);
 
-  private VictorSPX right1, right2, left1, left2;
-  private Joystick stick;
-  private UsbCamera camera;
-
-  @Override
-  public void robotInit() 
-  {
-    right1 = new VictorSPX(3);
-    right2 = new VictorSPX(4);
-    left1 = new VictorSPX(1);
-    left2 = new VictorSPX(2);
-
-    stick = new Joystick(0);
-  }
+  // Speed variables
+  double stopSpeed = 0.0;
+  double spinSpeed = 0.5; // Adjust this value for spin speed
+  double fastSpeed = 1.0; // Max forward speed
 
   @Override
-  public void robotPeriodic() {}
+  public void robotInit() {}
+
+  PhotonCamera camera = new PhotonCamera("CamFront");
 
   @Override
-  public void autonomousInit() {}
-
-  @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void teleopInit() 
-  {
-    camera = CameraServer.startAutomaticCapture();
-  }
-
-  @Override
-  public void teleopPeriodic() 
-  {
-    double x_val = stick.getRawAxis(1);
-    double y_val = stick.getRawAxis(0);
-
-    if(x_val>=1)
-      x_val = 0.99;
-    else if(x_val<=-1)
-      x_val = -0.99;
-
-    if(y_val>=1)
-      y_val = 0.99;
-    else if(y_val<=-1)
-      y_val = -0.99;
-
-    double turnOffset = x_val*y_val;
-
-    double rightSpeed = y_val;
-    double leftSpeed = y_val;
-
-    if(turnOffset<0)
-    {
-      rightSpeed = Math.abs(y_val)-Math.abs(turnOffset);
+  public void robotPeriodic() {
+    // Query the latest result from PhotonVision
+    List <PhotonPipelineResult> result = camera.getAllUnreadResults();
+    for(int i = 0; i<result.size(); i++){
+      if ((result.get(i)).hasTargets()) {
+        var target = (result.get(i)).getBestTarget();
+        double yaw = target.getYaw(); // Angle of the target
+        double distance = target.getBestCameraToTarget().getTranslation().getZ(); // Distance to the target
+        // Use this data for navigation or other purposes
+      }
     }
-    else if(turnOffset>0)
-    {
-      leftSpeed = Math.abs(y_val)-Math.abs(turnOffset);
-    }
-
-    left1.set(ControlMode.PercentOutput, -leftSpeed);
-    left2.set(ControlMode.PercentOutput, -leftSpeed);
-    right1.set(ControlMode.PercentOutput, rightSpeed);
-    right2.set(ControlMode.PercentOutput, rightSpeed);
   }
 
+
+  @Override
+  public void autonomousInit() {
+
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+
+  }
+
+  @Override
+  public void teleopInit() {
+    UsbCamera camera = CameraServer.startAutomaticCapture();
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    double forward = -joystick.getRawAxis(1); // Y-axis for forward/backward
+    double turn = joystick.getRawAxis(0); // X-axis for turning
+
+    double speed_left = forward + turn;
+    double speed_right = forward - turn;
+
+    // Clamp speeds to be within the range -1 to 1
+    speed_left = Math.max(-1.0, Math.min(1.0, speed_left));
+    speed_right = Math.max(-1.0, Math.min(1.0, speed_right));
+
+    // Set motor outputs
+    driveLeft1.set(ControlMode.PercentOutput, speed_left);
+    driveLeft2.set(ControlMode.PercentOutput, speed_left);
+    driveRight1.set(ControlMode.PercentOutput, speed_right);
+    driveRight2.set(ControlMode.PercentOutput, speed_right);
+}
+  
   @Override
   public void disabledInit() {}
 
