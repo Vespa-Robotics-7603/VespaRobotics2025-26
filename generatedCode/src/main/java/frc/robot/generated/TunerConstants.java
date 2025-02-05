0f8fdc0ee1a66f4b992c444dfde9c10d61d5592a
leejkc <notaoeu@tutanota.com>
2025-02-05 17:55:24 -0500

    Update robot current and K values

--- generatedCode/src/main/java/frc/robot/generated/TunerConstants.java
+++ generatedCode/src/main/java/frc/robot/generated/TunerConstants.java
@@ -24,13 +24,13 @@ public class TunerConstants {
     // The steer motor uses any SwerveModule.SteerRequestType control request with the
     // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
     private static final Slot0Configs steerGains = new Slot0Configs()
-        .withKP(10).withKI(0).withKD(2)
+        .withKP(6).withKI(0).withKD(2)
         .withKS(0.1).withKV(0).withKA(0)
         .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
     // When using closed-loop control, the drive motor uses the control
-    // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
+    // output type specified by SwerveModuleConstants.DriveMotorClos edLoopOutput
     private static final Slot0Configs driveGains = new Slot0Configs()
-        .withKP(1.5).withKI(1.5).withKD(2)
+        .withKP(10).withKI(4).withKD(10)
         .withKS(0).withKV(0.124);
 
     // The closed-loop output type to use for the steer motors;
@@ -51,7 +51,7 @@ public class TunerConstants {
 
     // The stator current at which the wheels start to slip;
     // This needs to be tuned to your individual robot
-    private static final Current kSlipCurrent = Amps.of(4);
+    private static final Current kSlipCurrent = Amps.of(13);
 
     // Initial configs for the drive and steer motors and the azimuth encoder; these cannot be null.
     // Some configs will be overwritten; check the `with*InitialConfigs()` API documentation.
