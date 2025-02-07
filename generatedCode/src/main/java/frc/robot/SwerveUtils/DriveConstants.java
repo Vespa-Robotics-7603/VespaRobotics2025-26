752d5534dd184d6808f1c68228cace70e50aec1f
notaoeu <notaoeu@tutanota.com>
2025-02-07 14:24:36 -0500

    Update constants in DriveConstants.java

--- generatedCode/src/main/java/frc/robot/SwerveUtils/DriveConstants.java
+++ generatedCode/src/main/java/frc/robot/SwerveUtils/DriveConstants.java
@@ -9,10 +9,4 @@ public class DriveConstants {
     public static final double KD = 15;
     public static final double KS = 0;
     public static final double KV = 0.124;
-
-    // Maximum rate of acceleration per second (i.e., by how much should we
-    // allow the requested X and Y values to change per second when speeding
-    // up from a stop)
-    // public static final double MAXACCELRATE = 0.5;
-    // public static SlewRateLimiter ACCELLIMITER = new SlewRateLimiter(MAXACCELRATE);
 }
