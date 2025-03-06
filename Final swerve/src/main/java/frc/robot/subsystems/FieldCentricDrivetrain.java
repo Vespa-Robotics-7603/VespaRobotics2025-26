1f3f2af6a5e4106571c9cd2669e7079f0c1dc436
notaoeu <notaoeu@tutanota.com>
2025-03-06 12:09:06 -0500

    Add inversion capabilities to FieldCentricDrivetrain

--- Final swerve/src/main/java/frc/robot/subsystems/FieldCentricDrivetrain.java
+++ Final swerve/src/main/java/frc/robot/subsystems/FieldCentricDrivetrain.java
@@ -7,20 +7,45 @@ import edu.wpi.first.math.filter.SlewRateLimiter;
 public class FieldCentricDrivetrain extends FieldCentric {
     private SlewRateLimiter xfilter = new SlewRateLimiter(10);
     private SlewRateLimiter yfilter = new SlewRateLimiter(10); 
+    private int isinverted;
     
     @Override
     public FieldCentric withVelocityX(double targetvelocity) {
-        double filteredvelocity;
-        filteredvelocity = (this.VelocityX < targetvelocity) ? xfilter.calculate(targetvelocity) : targetvelocity;
-        this.VelocityX = filteredvelocity;
+        double filteredvelocity = (
+            (this.VelocityX < targetvelocity)
+            ? xfilter.calculate(targetvelocity)
+            : targetvelocity
+        );
+
+        this.VelocityX = filteredvelocity * isinverted;
         return this;
     }
 
     @Override
     public FieldCentric withVelocityY(double targetvelocity) {
-        double filteredvelocity;
-        filteredvelocity = (this.VelocityY < targetvelocity) ? yfilter.calculate(targetvelocity) : targetvelocity;
-        this.VelocityY = filteredvelocity;
+        double filteredvelocity = (
+            (this.VelocityY < targetvelocity)
+            ? yfilter.calculate(targetvelocity)
+            : targetvelocity
+        );
+
+        this.VelocityY = filteredvelocity * isinverted;
         return this;
+    }
+
+    @Override
+    public FieldCentric withRotationalRate(double newRotationalRate) {
+        this.RotationalRate = newRotationalRate * isinverted;
+        return this;
+    }
+
+    public FieldCentricDrivetrain setInverted(Boolean isinverted) {
+        this.isinverted = (isinverted) ? -1 : 1;
+        return this;
+    }
+
+    public FieldCentricDrivetrain() {
+        super();
+        isinverted = 1;
     }
 }
