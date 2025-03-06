8a0ea388f9df1f8adf8810fb85625c0b626dca72
PriSey <psputtagunta@gmail.com>
2025-03-06 11:09:03 -0500

    Recommited elevator

---
+++ Final swerve/src/main/java/frc/robot/subsystems/FieldCentricDrivetrain.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.swerve.SwerveRequest.FieldCentric;
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
+
+public class FieldCentricDrivetrain extends FieldCentric {
+    private SlewRateLimiter xfilter = new SlewRateLimiter(10);
+    private SlewRateLimiter yfilter = new SlewRateLimiter(10);
+    
+    @Override
+    public FieldCentric withVelocityX(double targetvelocity) {
+        double filteredvelocity;
+        filteredvelocity = (this.VelocityX < targetvelocity) ? xfilter.calculate(targetvelocity) : targetvelocity;
+        this.VelocityX = filteredvelocity;
+        return this;
+    }
+
+    @Override
+    public FieldCentric withVelocityY(double targetvelocity) {
+        double filteredvelocity;
+        filteredvelocity = (this.VelocityY < targetvelocity) ? yfilter.calculate(targetvelocity) : targetvelocity;
+        this.VelocityY = filteredvelocity;
+        return this;
+    }
+}
