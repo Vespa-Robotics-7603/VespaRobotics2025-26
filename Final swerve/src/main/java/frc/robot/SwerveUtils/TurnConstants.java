576fa44d42dc096da57dc07449d358501477af23
Ethan Stark <ethan.jels2@gmail.com>
2025-03-06 07:03:21 -0500

    PID constants update (turn)
    Just the updated turn constants

--- Final swerve/src/main/java/frc/robot/SwerveUtils/TurnConstants.java
+++ Final swerve/src/main/java/frc/robot/SwerveUtils/TurnConstants.java
@@ -2,7 +2,7 @@ package frc.robot.SwerveUtils;
 
 public class TurnConstants {
     // K values
-    public static final double KP = 20;
+    public static final double KP = 10;
     public static final double KI = 0;
     public static final double KD = 0.1;
     public static final double KS = 0.1;
