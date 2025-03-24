75a16ca852c484315a325870b813b76d952ceed4
Ethan Stark <ethan.jels2@gmail.com>
2025-03-24 10:14:20 -0400

    Started to create constants for controller

---
+++ experimental/src/main/java/frc/robot/constants/ContollerCommands.java
@@ -0,0 +1,30 @@
+package frc.robot.constants;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class ContollerCommands {
+    //TODO, fill with commands for the controller
+    static {
+        //code can be put here to help set things up
+        //(This runs when the class is loaded, so we can use it to setup static constants)
+        
+    }
+    
+    public static Command POV_UP;
+    public static Command POV_DOWN;
+    public static Command POV_LEFT;
+    public static Command POV_RIGHT;
+    
+    public static Command LEFT_TRIGGER;
+    public static Command RIGHT_TRIGGER;
+    
+    public static Command LEFT_BUMPER;
+    public static Command RIGHT_BUMPER;
+    
+    public static Command A_BUTTON;
+    public static Command B_BUTTON;
+    public static Command X_BUTTON;
+    public static Command Y_BUTTON;
+    
+    public static Command START_BUTTON;
+}
