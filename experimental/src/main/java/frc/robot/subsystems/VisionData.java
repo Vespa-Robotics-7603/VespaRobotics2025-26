50a972ff575dfc8ab5d5d5f9916af4860933ab45
notaoeu <notaoeu@tutanota.com>
2025-03-24 18:37:46 -0400

    Preliminary refactoring of Vision.java

---
+++ experimental/src/main/java/frc/robot/subsystems/VisionData.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems;
+
+public class VisionData {
+    class VisionTarget {
+        double x;
+        double y;
+        double yaw;
+    }
+
+    class RobotPose {
+        double x;
+        double y;
+        double yaw;
+    }
+}
