74a8c1fc5b6ca64e771271bed78d858a48494ac3
Ethan Stark <ethan.jels2@gmail.com>
2025-03-27 10:04:09 -0400

    Added docs and constants for trajectory follower

---
+++ experimental/src/main/java/frc/robot/constants/TrajectoryConstants.java
@@ -0,0 +1,20 @@
+package frc.robot.constants;
+
+public class TrajectoryConstants{
+    public final static double MAX_SPEED = 10;
+    public final static double MAX_ACCELERATION = 3;
+    public final static double MAX_ROTATIONAL_SPEED = 4;
+    public final static double MAX_ROTATIONAL_ACCELERATION = 3.14; 
+
+    public final static double KP_X = 0.11;
+    public final static double KI_X = 0;
+    public final static double KD_X = 0;
+
+    public final static double KP_Y = 0.18;
+    public final static double KI_Y = 0;
+    public final static double KD_Y = 0;
+
+    public final static double KP_THETA = 11;
+    public final static double KI_THETA = 0;
+    public final static double KD_THETA = 0;
+}
