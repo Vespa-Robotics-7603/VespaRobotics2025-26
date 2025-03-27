3dc79756be278542e9173e0e0a61620f778a7f0f
lee <xlegendman@gmail.com>
2025-03-27 13:27:55 -0400

    new algae l2 l3 for liliana to practice with

--- experimental/src/main/java/frc/robot/constants/ElevatorConstants.java
+++ experimental/src/main/java/frc/robot/constants/ElevatorConstants.java
@@ -4,8 +4,8 @@ public class ElevatorConstants {
     public static final double INTAKE_POS = 100;
 
     public static final double GROUND_LEVEL = 0;
-    public static final double L2 = 105; // used to be 85
-    public static final double L3 = 200; // used to be 180
+    public static final double L2 = 105; // used to be 85 -> algae touching reef 105
+    public static final double L3 = 200; // used to be 180 -> algae touching reef 200
     // public static final double L4 = 320;
 
     //TODO: get actual max rotation please please please brian and team
